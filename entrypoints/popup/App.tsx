de83422463fa2622deec88e62706fe445dc6a71a
Poshkii <donatasrackauskass@gmail.com>
2025-02-23 13:01:07 +0200

    tips slides

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -1,25 +1,44 @@
-import { useState } from 'react';
+import { useState} from 'react';
 import './App.css';
 
+const slides = [
+  { id: 1, content: "Slaptažodžio ilgis ženkliai svarbiau nei simbolių sudėtingumas." },
+  { id: 2, content: "Slaptažodžiai turėtų būti minimaliai 8 simbolių ilgio." },
+  { id: 3, content: "Pakartoninai nevartoti slaptažodžių kurie buvo kompromituoti." },
+  { id: 4, content: "Vengti slaptažodyje vartoti įprastus žodyno žodžius." },
+  { id: 5, content: "Vengti slaptažodyje vartoti pasikartojančias ar nuspėjamas simbolių sekas ('aaaa' arba '1234' ir pan.)." },
+  { id: 6, content: "Vengti slaptaždoyje naudoti asmeninę informaciją (gimimo data, artimųjų vardai ir pan.)." },
+  { id: 7, content: "Saugoti slaptažodžius specializuotoje tvarkyklėje (password manager)." },
+  { id: 8, content: "Įvykus duomenų nutekėjimui nedelsiant keisti savo slaptažodį." },
+  { id: 9, content: "Nenaudoti identiškų slaptažodžių keliose skirtingose sistemose." },
+  { id: 10, content: "Nenaudoti slaptažodžio priminimo klausimų arba užuominų." },
+  { id: 11, content: "Apsaugoti savo prisijungimus 2FA." }
+];
+
 function App() {
-  const [count, setCount] = useState(0);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const nextSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % slides.length);
+  }
+  const prevSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex - 1 + slides.length) % slides.length);
+  }
 
   return (
     <>
-      <h2>Slaptažodžio saugumo <br></br>bendri patarimai</h2>
-      <ul>
-        <li>Slaptažodžio ilgis ženkliai svarbiau nei simbolių sudėtingumas.</li>
-        <li>Slaptažodžiai turėtų būti minimaliai 8 simbolių ilgio.</li>
-        <li>Pakartoninai nevartoti slaptažodžių kurie buvo kompromituoti.</li>
-        <li>Vengti slaptažodyje vartoti įprastus žodyno žodžius.</li>
-        <li>Vengti slaptažodyje vartoti pasikartojančias ar nuspėjamas simbolių sekas ('aaaa' arba '1234' ir pan.)</li>
-        <li>Vengti slaptaždoyje naudoti asmeninę informaciją (gimimo data, artimųjų vardai ir pan.)</li>
-        <li>Saugoti slaptažodžius specializuotoje tvarkyklėje (password manager).</li>
-        <li>Įvykus duomenų nutekėjimui nedelsiant keisti savo slaptažodį.</li>
-        <li>Nenaudoti identiškų slaptažodžių keliose skirtingose sistemose.</li>
-        <li>Nenaudoti slaptažodžio priminimo klausimų arba užuominų.</li>
-        <li>Apsaugoti savo prisijungimus 2FA.</li>
-      </ul>
+      <div>
+        <div style={{paddingBottom: '1rem'}}>
+          {slides.map((slide, index) => (
+            <div key={slide.id} style={{ display: index === currentIndex ? "block" : "none" }}>
+              <div>{slide.id} / {slides.length}</div>
+              <div style={{textAlign: 'left', paddingTop:'1rem'}}>{slide.content}</div>
+            </div>
+          ))}
+        </div>
+        <button style={{marginRight:'1rem'}} onClick={prevSlide}>❮</button>
+        <button onClick={nextSlide}>❯</button>
+      </div>
     </>
   );
 }
