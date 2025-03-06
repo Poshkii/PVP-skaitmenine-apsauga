fd19f78e49fbe776eed6ed5fce19d6bfa89a627a
dov-vai <160327869+dov-vai@users.noreply.github.com>
2025-03-06 10:45:49 +0200

    fix(Settings): bring back search bar

--- components/pages/settings/Settings.tsx
+++ components/pages/settings/Settings.tsx
@@ -1,12 +1,24 @@
 import PasswordCheckerOptions from "@/components/pages/settings/PasswordCheckerOptions.tsx";
+import {Search} from "lucide-react";
 
 const values = []
 
 function Settings(){
     return (
-      <div className="items-list">
-        <PasswordCheckerOptions/>
-      </div>
+      <>
+          <div className="top-bar">
+              <Search className="search-icon" size={18}/>
+              <input
+                  type="text"
+                  placeholder="Search"
+                  className="search-input"
+              />
+          </div>
+
+          <div className="items-list">
+              <PasswordCheckerOptions/>
+          </div>
+      </>
     );
 
 }
