04303ab69bdf1ff5ebcd360195af44c0ae20e824
dov-vai <160327869+dov-vai@users.noreply.github.com>
2025-04-02 10:25:22 +0300

    fix(home): use home routes with params

--- components/pages/home/Home.tsx
+++ components/pages/home/Home.tsx
@@ -1,67 +1,42 @@
-import {Route, Routes, useNavigate} from "react-router";
-
-import PasswordChecker from "@/components/pages/password-checker/PasswordChecker.tsx";
+import {useNavigate} from "react-router";
 import FeatureList from "@/components/pages/home/FeatureList.tsx";
-import SettingsPage from "@/components/pages/settings/Settings.tsx";
-import URLChecker from "../url-checker/URLChecker";
-import EmailChecker from "../email-checker/EmailChecker";
-import FileChecker from "@/components/pages/file-checker/FileChecker";
-import InfoPage from "../info-page/InfoPage";
-import CookiesManager from "@/components/pages/cookies/Cookies";
-
-import URLData from "../data-usage/URL/URLDataUsage";
-import FileData from "../data-usage/File/FileDataUsage";
-import PasswordData from "../data-usage/Password/PasswordDataUsage";
-import EmailData from "../data-usage/Email/EmailDataUsage";
 
 import {useBackgroundMessaging} from "@/hooks/useBackgroundMessaging.ts";
 
 import {Settings} from "lucide-react";
 import logo from "@/assets/icon.png";
+import HomeRoutes from "@/components/pages/home/HomeRoutes.tsx";
 
-function Home(){
+function Home() {
     useBackgroundMessaging();
     const navigate = useNavigate();
 
     return (
         <div className="main-window">
-          {/* Left Panel */}
+            {/* Left Panel */}
             <div className="left-panel">
                 <div className="title-container">
                     <img className="logo-image" src={logo} alt="FalconFort logo"/>
                     <h1 className="falcon-fort-title">FalconFort</h1>
                 </div>
-                <FeatureList />
-            
+                <FeatureList/>
+
                 {/* Settings button at bottom */}
                 <button className="settings-button "
-                    onClick={() => navigate("/settings")}>
+                        onClick={() => navigate("/settings")}>
                     <div className="settings-icon">
-                    <Settings />
+                        <Settings/>
                     </div>
                     <span className="settings-text">Settings</span>
                 </button>
             </div>
-          
-          {/* Right Panel */}
-          <div className="right-panel">
-            <Routes>
-              <Route path="/url-checker" element={<URLChecker />} />
-              <Route path="/file-checker" element={<FileChecker />} />
-              <Route path="/password-checker" element={<PasswordChecker />} />
-              <Route path="/cookies" element={<CookiesManager />} />
-              <Route path="/email-checker" element={<EmailChecker />} />
-              <Route path="/info-page" element={<InfoPage />} />
-              <Route path="/settings" element={<SettingsPage />} />
 
-              <Route path="/url-data" element={<URLData />}></Route>
-              <Route path="/file-data" element={<FileData />}></Route>
-              <Route path="/password-data" element={<PasswordData />}></Route>
-              <Route path="/email-data" element={<EmailData />}></Route>
-            </Routes>
-          </div>
+            {/* Right Panel */}
+            <div className="right-panel">
+                <HomeRoutes/>
+            </div>
         </div>
-      );
+    );
 
 }
 
