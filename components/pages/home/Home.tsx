463673d3e33bdf0b48c6de1aad7b6400afde55a0
Poshkii <donatasrackauskass@gmail.com>
2025-04-02 19:44:26 +0300

    report (home) page design changes

--- components/pages/home/Home.tsx
+++ components/pages/home/Home.tsx
@@ -3,7 +3,7 @@ import FeatureList from "@/components/pages/home/FeatureList.tsx";
 
 import {useBackgroundMessaging} from "@/hooks/useBackgroundMessaging.ts";
 
-import {Settings} from "lucide-react";
+import {Settings, House, User} from "lucide-react";
 import logo from "@/assets/icon.png";
 import HomeRoutes from "@/components/pages/home/HomeRoutes.tsx";
 
@@ -15,20 +15,37 @@ function Home() {
         <div className="main-window">
             {/* Left Panel */}
             <div className="left-panel">
-                <div className="title-container">
-                    <img className="logo-image" src={logo} alt="FalconFort logo"/>
-                    <h1 className="falcon-fort-title">FalconFort</h1>
+                <div className="title-container" onClick={() => navigate("/report-page")}>
+                    <img className="logo-image title-hover" src={logo} alt="FalconFort logo"/>
+                    <h1 className="falcon-fort-title title-hover">FalconFort</h1>
                 </div>
                 <FeatureList/>
 
                 {/* Settings button at bottom */}
-                <button className="settings-button "
-                        onClick={() => navigate("/settings")}>
-                    <div className="settings-icon">
-                        <Settings/>
-                    </div>
-                    <span className="settings-text">Settings</span>
-                </button>
+                <div className="bottom-buttons">
+                    <button className="bottom-button home-button"
+                            onClick={() => navigate("/report-page")}>
+                        <div>
+                            <House className="home-icon"/>
+                            <span className="bottom-text">Home</span>
+                        </div>
+                    </button>
+                    <button className="bottom-button profile-button"
+                            onClick={() => navigate("/profile")}>
+                        <div>
+                            <User className="profile-icon"/>
+                            <span className="bottom-text">Profile</span>
+                        </div>
+                    </button>
+                    <button className="bottom-button settings-button"
+                            onClick={() => navigate("/settings")}>
+                        <div>
+                            <Settings className="settings-icon"/>
+                            <span className="bottom-text">Settings</span>
+                        </div>
+                    </button>
+                </div>
+                
             </div>
 
             {/* Right Panel */}
--- components/pages/report-page/ReportPage.tsx
+++ components/pages/report-page/ReportPage.tsx
@@ -5,26 +5,37 @@ function ReportPage() {
 
     return (
         <div className="middle-menu">
-            <h1>Report Page</h1>
+            <h1 className="panel-title">Overview</h1>
 
-            <div style={{color: "white"}}>
-                <p><strong>URL Scans:</strong> {report.UrlScans}</p>
-                <p><strong>File Scans:</strong> {report.FileScans}</p>
-
-                <h2>5 Latest Scanned Emails:</h2>
-                {report.ScannedEmails.length === 0 ? (
-                    <p>No emails have been scanned so far...</p>
-                ) : (
-                    <ul>
-                        {report.ScannedEmails.slice(-5).reverse().map((email, index) => (
-                            <li key={index}>{email.email} - <strong>{email.BreachCount}</strong> breaches</li>
-                        ))}
-                    </ul>
-                )}
+            <div>
+                <div className="security-check-container">
+                    <h3 className="recent-list-title">Safety check statistics</h3>
+                    <div className="security-status">
+                        <p className="recent-item recent-item-alt"><strong className="recent-item-text">URL<br></br>Scans:</strong> <span className="status-icon">{report.UrlScans}</span></p>
+                        <p className="recent-item recent-item-alt"><strong className="recent-item-text">File<br></br> Scans:</strong> <span className="status-icon">{report.FileScans}</span></p>
+                    </div>
+                </div>
+                
+                <div className="security-check-container">
+                    <h3 className="recent-list-title">5 Last Scanned Emails:</h3>
+                    {report.ScannedEmails.length === 0 ? (
+                        <p>No emails have been scanned so far...</p>
+                    ) : (
+                        <ul className="recent-items" style={{paddingLeft: "0"}}>
+                            {report.ScannedEmails.slice(-5).reverse().map((email, index) => (
+                                <li className="recent-item" 
+                                    key={index}><span className="overflow-text">{email.email}</span> 
+                                    <span><strong>{email.BreachCount}</strong> breaches</span>
+                                </li>
+                            ))}
+                        </ul>
+                    )}
+                </div>
+                
             </div>
 
-            <button className="menu-button" onClick={clearReport}>
-                <p className="menu-name">Clear All Data</p>
+            <button className="btn btn-secondary" onClick={clearReport}>
+                Clear All Data
             </button>
         </div>
     );
