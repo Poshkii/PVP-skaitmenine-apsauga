90bbfca09ee0097e146997ded1ef429b4a86baf2
Poshkii <donatasrackauskass@gmail.com>
2025-04-07 11:35:47 +0300

    Password hints update

--- components/pages/password-checker/PasswordTips.tsx
+++ components/pages/password-checker/PasswordTips.tsx
@@ -3,14 +3,14 @@ import { Lightbulb } from 'lucide-react';
 
 const slides = [
     { id: 1, content: "The length of a password is significantly more important than its complexity.", img: "/pswd_images/complexity.png"  },
-    { id: 2, content: "Passwords should be at least 8 characters long.", img: "/pswd_images/length.png"  },
-    { id: 3, content: "Avoid using common dictionary words in passwords.", img: "/pswd_images/dictionary.png"  },
+    { id: 2, content: "Passwords should be minimum 8 characters long, preferably at least 16 characters long.", img: "/pswd_images/length.png"  },
+    { id: 3, content: "Avoid using common, simple and easy to guess dictionary words in passwords.", img: "/pswd_images/dictionary.png"  },
     { id: 4, content: "Do not use repeating or predictable sequences in passwords (e.g., 'aaaa' or '1234').", img: "/pswd_images/repetition.png"  },
     { id: 5, content: "Do not include personal information in passwords (e.g., birthdate, name, etc.).", img: "/pswd_images/personal.png"  },
-    { id: 6, content: "Store passwords in a specialized password manager.", img: "/pswd_images/managers.png"  },
+    { id: 6, content: "Store passwords in a specialized password manager.", img: "/pswd_images/manager.png"  },
     { id: 7, content: "Change your password immediately if a data breach occurs.", img: "/pswd_images/leaks.png"  },
-    { id: 8, content: "Do not reuse the same password across multiple systems.", img: "/pswd_images/reuse.png"  },
-    { id: 9, content: "Avoid using password reminder questions or hints.", img: "/pswd_images/hints.png"  },
+    { id: 8, content: "Do not reuse the same password across multiple systems, websites, etc.", img: "/pswd_images/reuse.png"  },
+    { id: 9, content: "Avoid using password recovery or reminder questions and hints.", img: "/pswd_images/hints.png"  },
     { id: 10, content: "Protect your logins by enabling two-factor authentication (2FA).", img: "/pswd_images/2fa.png"  }
 ];
 
