8fb67f0fe0a1f382cdb723f2a61e0844e624d7d9
Kristupelis <ckristupas@gmail.com>
2025-04-12 13:17:26 +0300

    Language selector
    added lng selector in settings

--- components/pages/settings/Settings.tsx
+++ components/pages/settings/Settings.tsx
@@ -13,6 +13,18 @@ function Settings() {
             <h1 className="panel-title">Settings</h1>
 
             <div className="security-check-container glassmorphism">
+                    <div className="setting-container">
+                <div className="setting-text">
+                    <p className="setting-title">Language</p>
+                    <p className="setting-description">Choose your preferred language</p>
+                </div>
+                <div className="language-selector">
+                    <select className="language-dropdown">
+                        <option value="en">English</option>
+                        <option value="lt">Lithuanian</option>
+                    </select>
+                </div>
+            </div>
                 <ModuleToggle
                     moduleId={ModuleId.PasswordChecker}
                     title={"Password Checker Button"}
