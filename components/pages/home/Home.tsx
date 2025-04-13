dc61eca332aad0cd784f78c4af11307f6534988d
dov-vai <160327869+dov-vai@users.noreply.github.com>
2025-04-13 19:34:32 +0300

    feat: logo clarity changes

--- components/pages/home/Home.tsx
+++ components/pages/home/Home.tsx
@@ -4,7 +4,6 @@ import FeatureList from "@/components/pages/home/FeatureList.tsx";
 import {useBackgroundMessaging} from "@/hooks/useBackgroundMessaging.ts";
 
 import {Settings, House, User} from "lucide-react";
-import logo from "@/assets/icon.png";
 import HomeRoutes from "@/components/pages/home/HomeRoutes.tsx";
 
 function Home() {
@@ -16,7 +15,7 @@ function Home() {
             {/* Left Panel */}
             <div className="left-panel">
                 <div className="title-container" onClick={() => navigate("/report-page")}>
-                    <img className="logo-image title-hover" src={logo} alt="FalconFort logo"/>
+                    <img className="logo-image title-hover" src="logo/logo.png" alt="FalconFort logo"/>
                     <h1 className="falcon-fort-title title-hover">FalconFort</h1>
                 </div>
                 <FeatureList/>
