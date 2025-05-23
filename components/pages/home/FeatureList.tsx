2cdd2c78e7a5082497ed863651c85880d08c8c28
Kristupelis <ckristupas@gmail.com>
2025-05-23 09:28:51 +0300

    Final

--- components/pages/home/FeatureList.tsx
+++ components/pages/home/FeatureList.tsx
@@ -41,14 +41,12 @@ function FeatureList() {
       route: "/trackers",
       icon: <Radar size={18} />
     },
-    /*
     { 
       nameKey: "phishEmail.name", 
       hintKey: "phishEmail.hint", 
       route: "/phish-email",
       icon: <Fish size={18} />
     },
-    */
     { 
       nameKey: "info.name", 
       hintKey: "info.hint", 
